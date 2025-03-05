0e299a22b93db7ddc659a5606d755729c14a636a
jsc <jsc@jsc.com>
2025-03-05 10:11:27 +0100

    improve command line interaction

--- lib/index.ts
+++ lib/index.ts
@@ -6,15 +6,47 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Do you want to run the script? (y/n)", (response) => {
-  if (response.toLowerCase() === "y") {
-    console.log("running script");
-    runScript();
-  } else if (response.toLowerCase() === "n") {
-    console.log("ok, not running the script");
-  } else {
-    console.log("Option not available!");
-  }
+const handleInput = (input: string) => {
+  const command = input.trim().toLowerCase();
 
-  rl.close();
-});
+  switch (command) {
+    case "exec-besu":
+      console.log("running script");
+      runScript();
+      rl.close();
+      break;
+    case "exit":
+      console.log("Goodbye!");
+      rl.close();
+      break;
+    case "help":
+      showHelp();
+      promptInput();
+      break;
+    default:
+      console.log(`Unknown command: ${command}`);
+      showHelp();
+      promptInput();
+  }
+};
+
+const promptInput = () => {
+  rl.question("> ", handleInput);
+};
+
+const showHelp = () => {
+  console.log("\nAvailable commands:");
+  console.log(" - help: Show available commands");
+  console.log(" - exec-besu: exec the besu script");
+  console.log(" - exit: Quit the program\n");
+};
+
+const initCommandLine = () => {
+  console.log("Hello from this command line, what would you like to do? ");
+
+  showHelp();
+  promptInput();
+};
+
+// Start the cl interaction
+initCommandLine();
